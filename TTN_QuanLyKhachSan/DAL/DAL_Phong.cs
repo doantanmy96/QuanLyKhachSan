5accdcca2b65d4799523d51e2a7aa157daaa8065
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-10-20 09:29:27 +0700

    d

--- TTN_QuanLyKhachSan/DAL/DAL_Phong.cs
+++ TTN_QuanLyKhachSan/DAL/DAL_Phong.cs
@@ -22,23 +22,6 @@ namespace DAL
             return connect.ThucHienLenh("insert into tblPhong(MaPh,SoPhong,TrangThai) values(N'" + EC_Ph.MaPh + "', N'" + EC_Ph.SoPhong + "', N'" + EC_Ph.TrangThai + "') ");
         }
 
-        public int SuaThongTin(EC_Phong EC_Ph)
-        {
-            return connect.ThucHienLenh("update tblPhong set SoPhong = N'" + EC_Ph.SoPhong + "', TrangThai = N'" + EC_Ph.TrangThai + "'  where MaPh = '" + EC_Ph.MaPh + "'");
-        }
 
-        public int XoaTTPhong(EC_Phong EC_Ph)
-        {
-            return connect.ThucHienLenh("delete from tblPhong where MaPh = '" + EC_Ph.MaPh + "'");
-        }
-        public DataTable TaoBang(string DieuKien)
-        {
-            return connect.GetDataTable(@"SELECT * FROM tblPhong " + DieuKien);
-        }
-
-        public void TraPhong(EC_Phong EC_Ph)
-        {
-            connect.ThucHienLenh("update tblPhong set TrangThai = N'" + EC_Ph.TrangThai + "'  where MaPh = '" + EC_Ph.MaPh + "'");
-        }
     }
 }
