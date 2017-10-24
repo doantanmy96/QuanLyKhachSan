f214d2c16f303453a685e23465105cfc9c159e17
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-10-24 21:01:54 +0700

    ad

--- TTN_QuanLyKhachSan/TTN_QuanLyKhachSan/frmPhong.cs
+++ TTN_QuanLyKhachSan/TTN_QuanLyKhachSan/frmPhong.cs
@@ -20,25 +20,6 @@ namespace TTN_QuanLyKhachSan
         private EC_Phong ec_Ph = new EC_Phong();
         private DataTable tblPhong = new DataTable();
         private bool themmoi;
-        public int SuaThongTin(EC_ThietBi EC_ThietBi)
-        {
-            return connect.ThucHienLenh("update tblThietBi set SoLuong='" + EC_ThietBi.SoLuong + "',NhaSanXuat=N'" + EC_ThietBi.NhaSanXuat + "' where MaPh=N'" + EC_ThietBi.MaPh + "' and ThietBi=N'" + EC_ThietBi.ThietBi + "'");
-        }
-
-        public int XoaTTThietBi(EC_ThietBi EC_ThietBi)
-        {
-            return connect.ThucHienLenh("delete from tblThietBi where MaPh=N'" + EC_ThietBi.MaPh + "' and ThietBi=N'" + EC_ThietBi.ThietBi + "'");
-
-        }
-        public DataTable TaoBang(string DieuKien)
-        {
-            return connect.GetDataTable(@"SELECT * FROM tblThietBi " + DieuKien);
-        }
-        public DataTable TruyVanRaMaPh(string DieuKien)
-        {
-            return connect.GetDataTable(@"SELECT MaPh from tblPhong " + DieuKien);
-        }
-
 
         private void SetNull()
         {
